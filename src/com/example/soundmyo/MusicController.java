61f602860f39ba190f142ce6a265f2547d9f08c0
sgwhack <gmark.sgw@gmail.com>
2014-09-29 22:31:49 -0400

    Initial Commit. Music Player Works.

---
+++ src/com/example/soundmyo/MusicController.java
@@ -0,0 +1,14 @@
+package com.example.soundmyo;
+
+import android.content.Context;
+import android.widget.MediaController;
+
+public class MusicController extends MediaController {
+
+	public MusicController(Context c){
+		super(c);
+	}
+		 
+	public void hide(){}
+	
+}
